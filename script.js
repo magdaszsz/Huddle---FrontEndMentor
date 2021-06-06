df46921674fdd2aee2e2a19d8fa627a19bd27d47
Magda Sz <pesahson@gmail.com>
2021-06-06 20:11:19 +0200

    add intersection observer

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-const sections = document.querySelectorAll('.reveal')
+const sections = document.querySelectorAll(".reveal");
 
-const ratio = .1;
+const ratio = 0.1;
 const options = {
   root: null,
-  rootMargin: '0px',
-  threshold: ratio
-}
+  rootMargin: "0px",
+  threshold: ratio,
+};
 
 const revealSection = (entries, observer) => {
   const [entry] = entries;
-  if(entry.intersectionRatio > ratio) {
-    entry.target.classList.remove('hidden');
-    observer.unobserve(entry.target)
+  if (entry.intersectionRatio > ratio) {
+    entry.target.classList.remove("hidden");
+    observer.unobserve(entry.target);
   }
- 
-}
-
+};
 
 const sectionObserver = new IntersectionObserver(revealSection, options);
 
-
-
-  sections.forEach(section =>  {
-    sectionObserver.observe(section);
-    section.classList.add('hidden');
-  })
+sections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("hidden");
+});
